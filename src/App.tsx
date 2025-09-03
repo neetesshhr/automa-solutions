416a2f72667a7f77b0b043588891307c8a9b7784
nitesh <104972681+sunnyrijal@users.noreply.github.com>
2025-09-03 12:34:58 +0545

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
-            <Route path="/sigtik" element={<SigTik />} />
+            {/* <Route path="/sigtik" element={<SigTik />} /> */}
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
